58e1586b6e26738c5120724f15445de87e8c2900
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2025-02-06 10:35:08 +0530

    db change

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(
+      "mongodb+srv://chandrashekharjoshi02:plXjxyNZkVBYFNYx@cluster0.lhux9.mongodb.net/user_management_node_react"
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error.message);
